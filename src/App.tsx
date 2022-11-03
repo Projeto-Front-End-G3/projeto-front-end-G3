09fa166e3781c61ec085025cf131f258952ec15d
Lucas <lucassoares2007@hotmail.com>
2022-11-03 15:05:28 -0300

    refactor: context

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { ToastContainer } from 'react-toastify';
 import GlobalStyle from './styles/global';
 import AppRouter from './routes/routes';
 import Providers from './contexts';
+import UserProviderNormal from './contexts/UserContextNormal';
 
 const App = () => (
   <>
     <GlobalStyle />
     <ToastContainer />
-    <Providers>
-      <AppRouter />
-    </Providers>
+    <UserProviderNormal>
+      <Providers>
+        <AppRouter />
+      </Providers>
+    </UserProviderNormal>
   </>
 );
 
