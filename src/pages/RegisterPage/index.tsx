bccae880a00b25b7f7a096e33949ca45f9d9763c
Kenji <moodzombie@gmail.com>
2022-11-04 12:54:59 -0300

    feat: add cep input to register

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -17,14 +17,17 @@ export interface iUserFormValue {
   passwordConfirm?: string;
   profilePicture: string;
   description: string;
+  cep: string;
   link: string;
 }
 
 const URL =
   /^((https?|ftp):\/\/)?(www.)?(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
 
+const cep = /^[0-9]{5}-[0-9]{3}$/;
+
 const RegisterPage = () => {
-  const { registerUser }: iUserContext = useContext(UserContext);
+  const { registerUser } = useContext(UserContext);
 
   const signUpFormSchema = yup.object().shape({
     name: yup.string().required("Campo obrigatório"),
@@ -46,6 +49,10 @@ const RegisterPage = () => {
       .required("Campo obrigatório")
       .matches(URL, "Coloque um link válido"),
     description: yup.string().required("Campo obrigatório"),
+    cep: yup
+      .string()
+      .matches(cep, "Coloque um CEP válido")
+      .required("Campo obrigatório"),
     link: yup
       .string()
       .required("Campo obrigatório")
@@ -126,6 +133,14 @@ const RegisterPage = () => {
             type="text"
             register={register}
             error={errors["description"]?.message}
+          />
+          <Input
+            name="cep"
+            label="CEP"
+            placeholder="Digite seu CEP"
+            type="text"
+            register={register}
+            error={errors["cep"]?.message}
           />
           <Input
             name="link"
