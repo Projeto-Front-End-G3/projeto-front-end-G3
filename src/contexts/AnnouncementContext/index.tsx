6621733d502d47df941c043352101ee1dd26e222
Vinicius Quirino <106350676+ViniciusQuirino@users.noreply.github.com>
2022-11-05 11:42:57 -0300

    GlobalLoading and ListCardAnnouncement

--- src/contexts/AnnouncementContext/index.tsx
+++ src/contexts/AnnouncementContext/index.tsx
@@ -1,13 +1,66 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { FaDoorClosed } from "react-icons/fa";
+import api from '../../services/api'
 
 type iAnnouncementProviderProps = {
   children: React.ReactNode;
 };
 
-export const AnnouncementContext = createContext({});
+interface iAnnouncementContext {
+  announcement: iAnnouncement[];
+  globalLoading: boolean;
+  setGlobalLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface iAnnouncement {
+  user: iUserDate
+  body: string;
+}
+
+interface iUserDate {
+  name: string;
+  img: string;
+}
+
+export const AnnouncementContext = createContext({} as iAnnouncementContext);
 
 const AnnouncementProvider = ({ children }: iAnnouncementProviderProps) => {
-  const value = {};
+
+  const [announcement, setAnnouncement] = useState<iAnnouncement[]>([])
+  const [globalLoading, setGlobalLoading] = useState(false)
+  console.log(globalLoading)
+  useEffect(() => {
+
+
+
+    const getAnnouncement = async () => {
+      setGlobalLoading(true)
+      try {
+
+        const response = await api.get('/announcement?_expand=user')
+        setAnnouncement(response.data)
+
+      } catch (error) {
+
+        console.error(error)
+
+      } finally {
+        setGlobalLoading(false)
+      }
+
+    }
+    getAnnouncement()
+
+
+  }, [])
+
+
+  const value = {
+    announcement,
+    globalLoading,
+    setGlobalLoading
+  }
+
   return (
     <AnnouncementContext.Provider value={value}>
       {children}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://projeto-frontend-api.herokuapp.com",
+  baseURL: "https://doisnomes.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
   },
